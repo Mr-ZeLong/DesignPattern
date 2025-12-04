9ed08def54f8f78c32c159a9822d5df4a44bf0f0
Mr-ZeLong <473840782@qq.com>
2025-12-04 23:48:03 +0800

    docs(strategy): 更新策略模式文档说明
    - 补充策略模式使用场景的描述，强调其在减少条件判断逻辑中的作用
    - 优化策略工厂实现方式的说明，明确区分简单工厂与工厂方法的使用条件
    - 增加对策略无状态时可使用缓存方式的建议
    - 完善对策略数量多且有状态情况下的工厂模式选择指导

--- src/main/java/com/zelon/designpattern/behavioral/strategy/OrderService.java
+++ src/main/java/com/zelon/designpattern/behavioral/strategy/OrderService.java
@@ -4,7 +4,9 @@ package com.zelon.designpattern.behavioral.strategy;
 策略模式
 一、使用场景
 在程序的运行过程中，需要根据参数决定接下来使用哪些算法/业务处理逻辑，
-会引入大量的if...else或者switch...case判断逻辑，此时可以使用策略模式解决
+会引入大量的if...else或者switch...case判断逻辑，此时可以使用策略模式
+将策略的定义、创建、使用分离，控制代码的复杂度，而且还能使得代码易于扩展，
+符合开闭原则。
 
 二、实现方式
 1. 策略定义
@@ -58,7 +60,8 @@ class NormalDiscountStrategy implements DiscountStrategy{
 
 // 创建策略
 // 如果策略是没有状态的，那么可以使用缓存的方式实现策略工厂，
-// 否则可以根据策略的数量，决定使用简单工厂还是工厂方法
+// 否则，少量的策略可以通过简单工厂实现，即便会出现少量的if-else也可以接受，
+// 但如果策略状态量多，那么就得使用工厂方法模式
 class DiscountStrategyFactory{
     private static final Map<String, DiscountStrategy> cachedStrategies = new HashMap<>();
     static {
